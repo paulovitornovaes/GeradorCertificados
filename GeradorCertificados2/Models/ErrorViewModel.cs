5701eb255c5ac7fd9b1c689fc081fb922f974ed0
Paulo Vitor <paulonovaes@id.uff.br>
2024-01-17 17:38:02 -0300

    adição do MVC com AlunoViewModel.cs incluso

---
+++ GeradorCertificados2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GeradorCertificados2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
